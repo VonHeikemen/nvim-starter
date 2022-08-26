4e6a5ab4f979ba8548a015ef7cfc3ac76c92a1e3
Heiker Curiel <heikercuriel@gmail.com>
2022-08-26 11:07:32 -0400

    add init.lua

---
+++ init.lua
@@ -0,0 +1,292 @@
+-- ========================================================================== --
+-- ==                           EDITOR SETTINGS                            == --
+-- ========================================================================== --
+
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.wrap = true
+vim.opt.breakindent = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = false
+vim.opt.signcolumn = 'yes'
+
+-- Space as leader key
+vim.g.mapleader = ' '
+
+-- Shortcuts
+vim.keymap.set({'n', 'x', 'o'}, '<leader>h', '^') 
+vim.keymap.set({'n', 'x', 'o'}, '<leader>l', 'g_')
+vim.keymap.set('n', '<leader>a', ':keepjumps normal! ggVG<cr>')
+
+-- Basic clipboard interaction
+vim.keymap.set({'n', 'x'}, 'cp', '"+y')
+vim.keymap.set({'n', 'x'}, 'cv', '"+p')
+
+-- Delete text
+vim.keymap.set({'n', 'x'}, 'x', '"_x')
+
+-- Commands
+vim.keymap.set('n', '<leader>w', '<cmd>write<cr>')
+vim.keymap.set('n', '<leader>bq', '<cmd>bdelete<cr>')
+vim.keymap.set('n', '<leader>bl', '<cmd>buffer #<cr>')
+
+
+-- ========================================================================== --
+-- ==                               COMMANDS                               == --
+-- ========================================================================== --
+
+vim.api.nvim_create_user_command(
+  'ReloadConfig',
+  'source $MYVIMRC | PackerCompile',
+  {}
+)
+
+local group = vim.api.nvim_create_augroup('user_cmds', {clear = true})
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight on yank',
+  group = group,
+  callback = function()
+    vim.highlight.on_yank({higroup = 'Visual', timeout = 200})
+  end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = {'help', 'man'},
+  group = group,
+  command = 'nnoremap <buffer> q <cmd>quit<cr>'
+})
+
+
+-- ========================================================================== --
+-- ==                               PLUGINS                                == --
+-- ========================================================================== --
+
+local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local install_plugins = false
+
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  print('Installing packer...')
+  local packer_url = 'https://github.com/wbthomason/packer.nvim'
+  vim.fn.system({'git', 'clone', '--depth', '1', packer_url, install_path})
+  print('Done.')
+
+  vim.cmd('packadd packer.nvim')
+  install_plugins = true
+end
+
+require('packer').startup(function(use)
+    -- Plugin manager
+  use {'wbthomason/packer.nvim'}
+
+  -- Theming
+  use {'folke/tokyonight.nvim'}
+  use {'joshdick/onedark.vim'}
+  use {'tanvirtin/monokai.nvim'}
+  use {'lunarvim/darkplus.nvim'}
+  use {'kyazdani42/nvim-web-devicons'}
+  use {'nvim-lualine/lualine.nvim'}
+  use {'akinsho/bufferline.nvim'}
+  use {'lukas-reineke/indent-blankline.nvim'}
+
+  -- File explorer
+  use {'kyazdani42/nvim-tree.lua', tag = 'nightly'}
+
+  -- Fuzzy finder
+  use {'nvim-telescope/telescope.nvim'}
+  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make'}
+
+  -- Git
+  use {'lewis6991/gitsigns.nvim'}
+  use {'tpope/vim-fugitive'}
+
+  -- Code manipulation
+  use {'nvim-treesitter/nvim-treesitter'}
+  use {'nvim-treesitter/nvim-treesitter-textobjects'}
+  use {'numToStr/Comment.nvim'}
+  use {'tpope/vim-surround'}
+  use {'wellle/targets.vim'}
+  use {'tpope/vim-repeat'}
+
+  -- Utilities
+  use {'moll/vim-bbye'}
+  use {'nvim-lua/plenary.nvim'}
+  use {'editorconfig/editorconfig-vim'}
+
+  if install_plugins then
+    require('packer').sync()
+  end
+end)
+
+if install_plugins then
+  print '=================================='
+  print '    Plugins are being installed'
+  print '    Wait until Packer completes,'
+  print '       then restart nvim'
+  print '=================================='
+  return
+end
+
+
+-- ========================================================================== --
+-- ==                         PLUGIN CONFIGURATION                         == --
+-- ========================================================================== --
+
+---
+-- Colorscheme
+---
+vim.opt.termguicolors = true
+vim.cmd('colorscheme onedark')
+
+
+---
+-- vim-bbye
+---
+vim.keymap.set('n', '<leader>bc', '<cmd>Bdelete<CR>')
+
+
+---
+-- lualine.nvim (statusline)
+---
+vim.opt.showmode = false
+
+-- See :help lualine.txt
+require('lualine').setup({
+  options = {
+    theme = 'onedark',
+    icons_enabled = true,
+    component_separators = '|',
+    section_separators = '',
+  },
+})
+
+
+---
+-- bufferline
+---
+-- See :help bufferline-settings
+require('bufferline').setup({
+  options = {
+    mode = 'buffers',
+    offsets = {
+      {filetype = 'NvimTree'}
+    },
+  }
+  -- :help bufferline-highlights
+  highlights = {
+    buffer_selected = {
+      gui = 'NONE'
+    },
+    indicator_selected = {
+      guifg = {attribute = 'fg', highlight = 'Function'},
+      gui = 'NONE'
+    }
+  }
+})
+
+
+---
+-- Treesitter
+---
+-- See :help nvim-treesitter-modules
+require('nvim-treesitter.configs').setup({
+  highlight = {
+    enable = true,
+  },
+  -- :help nvim-treesitter-textobjects-modules
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
+      }
+    },
+  },
+  ensure_installed = {
+    'javascript',
+    'typescript',
+    'tsx',
+    'lua',
+    'css',
+    'json'
+  },
+})
+
+
+---
+-- Comment.nvim
+---
+require('Comment').setup({})
+
+
+---
+-- Indent-blankline
+---
+-- See :help indent-blankline-setup
+require('indent_blankline').setup({
+  char = '▏',
+  show_trailing_blankline_indent = false,
+  show_first_indent_level = false,
+  use_treesitter = true,
+  show_current_context = false
+})
+
+
+---
+-- Gitsigns
+---
+-- See :help gitsigns-usage
+require('gitsigns').setup({
+  signs = {
+    add = {text = '▎'},
+    change = {text = '▎'},
+    delete = {text = '➤'},
+    topdelete = {text = '➤'},
+    changedelete = {text = '▎'},
+  }
+})
+
+
+---
+-- Telescope
+---
+-- See :help telescope.builtin
+vim.keymap.set('n', '<leader>?', '<cmd>Telescope oldfiles<cr>')
+vim.keymap.set('n', '<leader><space>', '<cmd>Telescope buffers<cr>')
+vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
+vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
+vim.keymap.set('n', '<leader>fd', '<cmd>Telescope diagnostics<cr>')
+vim.keymap.set('n', '<leader>fs', '<cmd>Telescope current_buffer_fuzzy_find<cr>')
+
+require('telescope').load_extension('fzf')
+
+
+---
+-- nvim-tree (File explorer)
+---
+-- See :help nvim-tree-setup
+require('nvim-tree').setup({
+  hijack_cursor = false,
+  on_attach = function(bufnr)
+    local bufmap = function(lhs, rhs, desc)
+      vim.keymap.set('n', lhs, rhs, {buffer = bufnr, desc = desc})
+    end
+
+    -- :help nvim-tree.api
+    local api = require('nvim-tree.api')
+   
+    bufmap('L', api.node.open.edit, 'Expand folder or go to file')
+    bufmap('H', api.node.navigate.parent_close, 'Close parent folder')
+  end
+})
+
+vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<cr>')
+
