33ed03d57c399061b54b906abbe2e79550456e5b
Heiker Curiel <heikercuriel@gmail.com>
2022-08-25 21:55:37 -0400

    add init.lua

---
+++ init.lua
@@ -0,0 +1,130 @@
+-- ========================================================================== --
+-- ==                           EDITOR SETTINGS                            == --
+-- ========================================================================== --
+
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.wrap = true
+vim.opt.breakindent = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = false
+
+
+-- ========================================================================== --
+-- ==                             KEYBINDINGS                              == --
+-- ========================================================================== --
+
+-- Space as leader key
+vim.g.mapleader = ' '
+
+-- Shortcuts
+vim.keymap.set({'n', 'x', 'o'}, '<leader>h', '^') 
+vim.keymap.set({'n', 'x', 'o'}, '<leader>l', 'g_')
+vim.keymap.set('n', '<leader>a', ':keepjumps normal! ggVG<cr>')
+
+-- Basic clipboard interaction
+vim.keymap.set({'n', 'x'}, 'cp', '"+y')
+vim.keymap.set({'n', 'x'}, 'cv', '"+p')
+
+-- Delete text
+vim.keymap.set({'n', 'x'}, 'x', '"_x')
+
+-- Commands
+vim.keymap.set('n', '<leader>w', '<cmd>write<cr>')
+vim.keymap.set('n', '<leader>bq', '<cmd>bdelete<cr>')
+vim.keymap.set('n', '<leader>bl', '<cmd>buffer #<cr>')
+
+
+-- ========================================================================== --
+-- ==                               COMMANDS                               == --
+-- ========================================================================== --
+
+vim.api.nvim_create_user_command(
+  'ReloadConfig',
+  'source $MYVIMRC | PackerCompile',
+  {}
+)
+
+local group = vim.api.nvim_create_augroup('user_cmds', {clear = true})
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight on yank',
+  group = group,
+  callback = function()
+    vim.highlight.on_yank({higroup = 'Visual', timeout = 200})
+  end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = {'help', 'man'},
+  group = group,
+  command = 'nnoremap <buffer> q <cmd>quit<cr>'
+})
+
+-- ========================================================================== --
+-- ==                               PLUGINS                                == --
+-- ========================================================================== --
+
+local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local install_plugins = false
+
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  print('Installing packer...')
+  local packer_url = 'https://github.com/wbthomason/packer.nvim'
+  vim.fn.system({'git', 'clone', '--depth', '1', packer_url, install_path})
+  print('Done.')
+
+  vim.cmd('packadd packer.nvim')
+  install_plugins = true
+end
+
+require('packer').startup(function(use)
+  use {'wbthomason/packer.nvim'}
+  use {'joshdick/onedark.vim'}
+  use {'kyazdani42/nvim-web-devicons'}
+  use {'nvim-lualine/lualine.nvim'}
+
+  if install_plugins then
+    require('packer').sync()
+  end
+end)
+
+if install_plugins then
+  print '=================================='
+  print '    Plugins are being installed'
+  print '    Wait until Packer completes,'
+  print '       then restart nvim'
+  print '=================================='
+  return
+end
+
+-- ========================================================================== --
+-- ==                         PLUGIN CONFIGURATION                         == --
+-- ========================================================================== --
+
+---
+-- Colorscheme
+---
+
+vim.opt.termguicolors = true
+vim.cmd('colorscheme onedark')
+
+
+---
+-- lualine.nvim (statusline)
+---
+
+vim.opt.showmode = false
+require('lualine').setup({
+  options = {
+    icons_enabled = false,
+    theme = 'onedark',
+    component_separators = '|',
+    section_separators = '',
+  },
+})
+
