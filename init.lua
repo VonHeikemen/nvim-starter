644a8c581eca210ec0ce1a5cb06db220b1cf6a3f
Heiker Curiel <heikercuriel@gmail.com>
2022-08-26 15:15:41 -0400

    update configuration

--- init.lua
+++ init.lua
@@ -25,13 +25,16 @@ vim.g.netrw_winsize = 30
 vim.g.mapleader = ' '
 
 -- Shortcuts
-vim.keymap.set({'n', 'x', 'o'}, '<leader>h', '^') 
+vim.keymap.set({'n', 'x', 'o'}, '<leader>h', '^')
 vim.keymap.set({'n', 'x', 'o'}, '<leader>l', 'g_')
 vim.keymap.set('n', '<leader>a', ':keepjumps normal! ggVG<cr>')
 
 -- Basic clipboard interaction
-vim.keymap.set({'n', 'x', 'o'}, 'cp', '"+y')
-vim.keymap.set({'n', 'x', 'o'}, 'cv', '"+p')
+vim.keymap.set({'n', 'x'}, 'cp', '"+y')
+vim.keymap.set({'n', 'x'}, 'cv', '"+p')
+
+-- Delete text
+vim.keymap.set({'n', 'x'}, 'x', '"_x')
 
 -- Commands
 vim.keymap.set('n', '<leader>w', '<cmd>write<cr>')
