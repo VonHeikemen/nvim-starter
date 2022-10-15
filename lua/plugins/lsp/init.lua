4e7ccba7e69182f3dc1ee2f27be66677118c35b6
Heiker Curiel <heikercuriel@gmail.com>
2022-10-15 08:20:51 -0400

    fix: update cmp-nvim-lsp config

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -19,9 +19,7 @@ local lsp_defaults = {
   flags = {
     debounce_text_changes = 150,
   },
-  capabilities = require('cmp_nvim_lsp').update_capabilities(
-    vim.lsp.protocol.make_client_capabilities()
-  ),
+  capabilities = require('cmp_nvim_lsp').default_capabilities(),
   on_attach = function(client, bufnr)
     vim.api.nvim_exec_autocmds('User', {pattern = 'LspAttached'})
   end
