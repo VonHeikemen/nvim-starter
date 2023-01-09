6b935e55b25892517a9f5e468c44e9e3a46a6a31
Heiker Curiel <heikercuriel@gmail.com>
2023-01-09 08:24:33 -0400

    refactor: specify lsp-zero dependencies

--- init.lua
+++ init.lua
@@ -9,24 +9,41 @@ local ok, packer = pcall(require, 'packer')
 if not ok then return end
 
 packer.startup(function(use)
-  use {'wbthomason/packer.nvim'}            -- Plugin manager
-  use {'folke/tokyonight.nvim'}             -- Colorscheme
-  use {'williamboman/mason.nvim'}           -- Installer for external tools
-  use {'williamboman/mason-lspconfig.nvim'} -- mason extension for LSP
-  use {'neovim/nvim-lspconfig'}             -- LSP server configurations
-  use {'hrsh7th/nvim-cmp'}                  -- Autocomplete engine
-  use {'hrsh7th/cmp-nvim-lsp'}              -- Completion source
-  use {'saadparwaiz1/cmp_luasnip'}          -- Completion source
-  use {'hrsh7th/cmp-buffer'}                -- Completion source
-  use {'hrsh7th/cmp-path'}                  -- Completion source
-  use {'L3MON4D3/LuaSnip'}                  -- Snippet engine
-  use {'rafamadriz/friendly-snippets'}      -- Snippet collection
-  use {'VonHeikemen/lsp-zero.nvim'}         -- Preconfigured Autocomplete + LSP setup
+  -- Plugin manager
+  use {'wbthomason/packer.nvim'}
+
+  -- Colorscheme
+  use {'folke/tokyonight.nvim'}
+
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
+
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},
+      -- Snippet Collection (Optional)
+      {'rafamadriz/friendly-snippets'},
+    }
+  }
 end)
 
 vim.opt.termguicolors = true
 vim.cmd('colorscheme tokyonight')
 
+-- Learn the keybindings, see :help lsp-zero-keybindings
+-- Learn to configure LSP servers, see :help lsp-zero-api-showcase
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
