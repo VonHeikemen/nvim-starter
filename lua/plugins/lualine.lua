d075b889a6670605db7b37b27419dfd56b98ea49
Heiker Curiel <heikercuriel@gmail.com>
2023-02-15 21:08:38 -0400

    feat: disable lualine in nvim-tree

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -9,6 +9,9 @@ Plugin.opts = {
     icons_enabled = true,
     component_separators = '|',
     section_separators = '',
+    disabled_filetypes = {
+      statusline = {'NvimTree'}
+    }
   },
 }
 
