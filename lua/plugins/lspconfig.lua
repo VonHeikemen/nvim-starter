a1db8aac64990d2508eec98227c8a681d4a04e1b
Heiker Curiel <heikercuriel@gmail.com>
2023-02-15 21:17:00 -0400

    refactor: move lsp keymaps

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -42,6 +42,31 @@ function Plugin.init()
   )
 end
 
+local function on_attach()
+  local bufmap = function(mode, lhs, rhs)
+    local opts = {buffer = true}
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+
+  -- You can search each function in the help page.
+  -- For example :help vim.lsp.buf.hover()
+
+  bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+  bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
+  bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+  bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
+  bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
+  bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
+  bufmap('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
+  bufmap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>')
+  bufmap('n', '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>')
+  bufmap('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+  bufmap('x', '<F4>', '<cmd>lua vim.lsp.buf.range_code_action()<cr>')
+  bufmap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+  bufmap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+  bufmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
+end
+
 function Plugin.config()
   -- See :help lspconfig-global-defaults
   local lspconfig = require('lspconfig')
@@ -52,6 +77,14 @@ function Plugin.config()
     lsp_defaults.capabilities,
     require('cmp_nvim_lsp').default_capabilities()
   )
+
+  local group = vim.api.nvim_create_augroup('lsp_cmds', {clear = true})
+
+  vim.api.nvim_create_autocmd('LspAttach', {
+    group = group,
+    desc = 'LSP actions',
+    callback = on_attach
+  })
 
   -- See :help mason-lspconfig-dynamic-server-setup
   require('mason-lspconfig').setup_handlers({
@@ -75,3 +108,4 @@ function Plugin.config()
 end
 
 return Plugin
+
--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,5 +1,3 @@
-local group = vim.api.nvim_create_augroup('mapping_cmds', {clear = true})
-
 -- Space as leader key
 vim.g.mapleader = ' '
 
@@ -20,32 +18,4 @@ vim.keymap.set('n', '<leader>w', '<cmd>write<cr>')
 vim.keymap.set('n', '<leader>bq', '<cmd>bdelete<cr>')
 vim.keymap.set('n', '<leader>bl', '<cmd>buffer #<cr>')
 
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = group,
-  desc = 'LSP actions',
-  callback = function()
-    local bufmap = function(mode, lhs, rhs)
-      local opts = {buffer = true}
-      vim.keymap.set(mode, lhs, rhs, opts)
-    end
-
-    -- You can search each function in the help page.
-    -- For example :help vim.lsp.buf.hover()
-
-    bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
-    bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
-    bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
-    bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
-    bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
-    bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
-    bufmap('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
-    bufmap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>')
-    bufmap('n', '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>')
-    bufmap('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
-    bufmap('x', '<F4>', '<cmd>lua vim.lsp.buf.range_code_action()<cr>')
-    bufmap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
-    bufmap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
-    bufmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
-  end
-})
 
