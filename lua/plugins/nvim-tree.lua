1d7e3d5f0f3d734cbebc90991fcedd1964f2c7f8
Heiker Curiel <heikercuriel@gmail.com>
2024-05-01 09:23:28 -0400

    fix - apply nvim-tree default mappings

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -13,6 +13,9 @@ Plugin.opts = {
     -- :help nvim-tree.api
     local api = require('nvim-tree.api')
 
+    -- default mappings
+    api.config.mappings.default_on_attach(bufnr)
+
     bufmap('L', api.node.open.edit, 'Expand folder or go to file')
     bufmap('H', api.node.navigate.parent_close, 'Close parent folder')
     bufmap('gh', api.tree.toggle_hidden_filter, 'Toggle hidden files')
