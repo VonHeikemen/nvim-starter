4bc65ba42186a557d60bda3158ef231879a3bb4b
Heiker Curiel <heikercuriel@gmail.com>
2025-10-15 20:35:00 -0400

    feat: add winborder

--- init.lua
+++ init.lua
@@ -11,6 +11,7 @@ vim.o.breakindent = true
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.expandtab = false
+vim.o.winborder = 'rounded'
 
 -- ========================================================================== --
 -- ==                             KEYBINDINGS                              == --
