9c50840925855692bc8641431b349a2e31ef1bb0
Heiker Curiel <heikercuriel@gmail.com>
2026-02-13 07:57:44 -0400

    feat: remove snacks bigfile

--- plugin/snacks.lua
+++ plugin/snacks.lua
@@ -29,20 +29,6 @@ MiniDeps.now(function()
         file = {truncate = 78},
       },
     },
-    bigfile = {
-      -- Only use `bigfile` module on older Neovim versions
-      enabled = vim.fn.has('nvim-0.11') == 0,
-      notify = false,
-      size = 1024 * 1024, -- 1MB
-      setup = function(ctx)
-        vim.cmd('syntax clear')
-        vim.opt_local.syntax = 'OFF'
-        local buffer = vim.b[ctx.buf]
-        if buffer.ts_highlight then
-          vim.treesitter.stop(ctx.buf)
-        end
-      end
-    },
   })
 
   -- Disable indent guide animation
